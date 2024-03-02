fe685c2a599627139256cf28773b110fa1557f7b
VishwesH <vishweshgupta30890@gmail.com>
2024-03-02 06:22:26 -0800

    a

--- src/components/LoginComponent/login.js
+++ src/components/LoginComponent/login.js
@@ -35,7 +35,7 @@ export default function Login(){
             {/* <h1>Create an account</h1> */}
             <div className="row innerBox">
             <div className='col-lg-12 logo'>
-                    <img src={process.env.PUBLIC_URL + '/images/thumbnails/logo.svg'}/>
+                    <img src='./images/thumbnails/logo.svg'/>
                     {/* <h6>Create an account</h6> */}
                 </div>
             <div className='col-lg-12'>
